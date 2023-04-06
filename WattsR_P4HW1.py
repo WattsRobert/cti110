f5f11d02bf32d8ec02dffb6cc8f6810452a76d04
WattsRobert <91197142+WattsRobert@users.noreply.github.com>
2023-04-06 13:22:29 -0400

    Add files via upload
    Original Upload (Incomplete)

---
+++ WattsR_P4HW1.py
@@ -0,0 +1,14 @@
+# CTI-110
+# P4HW1 - Score List
+# Robert Watts
+# Date
+#
+
+#Sets scores and other variables to 0
+scores = 0
+scale = 0
+
+scores = int(input('How many scores do you want to enter?'))
+
+while scales != 0
+    if 

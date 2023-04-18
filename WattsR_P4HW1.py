5e6a930b1c6aa27d7dae51d95df74043a8c59f9f
WattsRobert <91197142+WattsRobert@users.noreply.github.com>
2023-04-18 14:07:43 -0400

    Update WattsR_P4HW1.py
    Final Upload
    Upload of work done in class

--- WattsR_P4HW1.py
+++ WattsR_P4HW1.py
@@ -1,19 +1,54 @@
 # CTI-110
 # P4HW1 - Score List
 # Robert Watts
-# Date
+# 4/18/2023
 #
 
-#Sets scores and other variables to 0
-scores = 0
-scale = 0
+# sets function for ALL calculations done in program
+def main():
+    grade = 0
+    total = 0
+    grade_list = []
 
-scores = int(input('How many scores do you want to enter?'))
+    # asks for amount of scores to be inputed
+    num = int(input("How many scores do you want to enter: "))
+    print()
 
-if scores <= 0:
-    print("At least one score must be entered")
-else:
-    while scores >= scores:
-        scores = scores - 1
-        input("Enter score #", scores)
+    # for loop checking
+    for i in range(0, num):
+        grade = float(input("Enter score #" + str(i + 1) + ": "))
+        while grade < 0 or grade > 100:
+            print()
+            print("INVALID Score entered!!!")
+            print("Score should be between 0 and 100")
+            grade = float(input("Enter score # " + str(i + 1) + " again: "))
+        grade_list.append(grade)
+    print()
+
+    # variables set for lowest grade and average of grades minus the lowest one
+    lowest = min(grade_list)
+    grade_list.remove(lowest)
+    average = sum(grade_list) / len(grade_list)
+
+# calculate Letter Grade for Average Grade
+    if average >= 90:
+        grade = "A"
+    elif average >= 80:
+        grade = "B"
+    elif average >= 70:
+        grade = "C"
+    elif average >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+
+# prints results of calculations
+    print("------------Results--------------")
+    print("Lowest Score         : ", lowest)
+    print("Modified List         : ", grade_list)
+    print("Scores Average      : ", average)
+    print("Grade                    : ", grade)
+    print("-----------------------------------")
+
     
+main()

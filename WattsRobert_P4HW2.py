77386df0cd05a24b69ce78f206324e1bc9682d8a
WattsRobert <91197142+WattsRobert@users.noreply.github.com>
2023-04-20 14:14:13 -0400

    Update WattsRobert_P4HW2.py
    Added stuff, fixed other stuff

--- WattsRobert_P4HW2.py
+++ WattsRobert_P4HW2.py
@@ -4,22 +4,24 @@
 # P4HW2
 #
 
+# Calculations to copy and paste:
+# regular_pay = hours_worked * pay_rate (if no overtime hours)
+# overtime_pay = overtime_hours * overtime_rate (if overtime hours)
+# gross_pay = regular_pay + overtime_pay (if overtime hours)
+
 # Variable defining for Totals and user_name
 employees = 0
 overtime_pay_total = 0
 regular_pay_total = 0
 gross_pay_total = 0
-user_name = ''
 
-# Initial inputs:
+# Initial Loop Start
 user_name = input("Enter employee's name or Done to terminate: ")
-hours_worked = input("How many hours did ", user_name, " work?: ")
-pay_rate = input("What is ", user_name, "'s pay rate: ")
 
 # While loop to repeat until "Done" or "done" is entered
 while user_name != "Done" or user_name != "done":
     
-    # Setting up blank variables for use:
+    # Variable defining for Employee Information
     overtime_rate = 1.5
     overtime_hours = 0
     overtime_pay = 0
@@ -27,9 +29,8 @@ while user_name != "Done" or user_name != "done":
     gross_pay = 0
 
     # Entered information
-    user_name = input("Enter employee's name or Done to terminate: ")
-    hours_worked = input("How many hours did ", user_name, " work?: ")
-    pay_rate = input("What is ", user_name, "'s pay rate: ")
+    hours_worked = int(input("How many hours did " + user_name + " work?: "))
+    pay_rate = int(input("What is " + user_name + "'s pay rate: "))
     
     # if statement for overtime pay if applicable
     if hours_worked > 40:
@@ -49,10 +50,13 @@ while user_name != "Done" or user_name != "done":
     regular_pay_total += regular_pay
     gross_pay_total += gross_pay
     
-    # prints Employee numbers out
-    print("Employee name: ", )
+    # Prints Employee numbers out
+    print("Employee name: ", user_name)
     print("Hours Worked", "Pay Rate", "OverTime", "OverTime Pay", "RegHour Pay", "Gross Pay")
-    print("")
+    print(hours_worked, pay_rate, overtime_hours, overtime_pay, regular_pay, gross_pay)
+
+    # Restarts loop by getting input
+    user_name = input("Enter employee's name or Done to terminate: ")
 
 # Prints Total Employee numbers out
 print("Total number of employees entered: ", employees)

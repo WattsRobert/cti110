cd20ddf73b172277da51b414a425b841dce65538
WattsRobert <91197142+WattsRobert@users.noreply.github.com>
2023-05-02 13:49:17 -0400

    Update P5HW_WattsRobert.py
    Fully functioning menu, no viable functions yet

--- P5HW_WattsRobert.py
+++ P5HW_WattsRobert.py
@@ -4,17 +4,19 @@
 # Robert Watts
 #
 
+##### Defining functions and variables
+# variables
 option = 0
 
-print("Welcome to Math Quiz")
-print()
-
+# adding function
 def adding():
     print('adding test')
 
+# subtraction function
 def subtracting():
     print('subtracting test')
 
+# Menu function (not being used rn)
 def Math_Quiz():
     print()
     print("MAIN MENU")
@@ -25,11 +27,40 @@ def Math_Quiz():
     print()
     option = input(int("Please choose one of the menu options: "))
 
+##### Program starts
+print("Welcome to Math Quiz!")
+print()
+print("MAIN MENU")
+print("-" * 30)
+print("1. Adding Random Numbers")
+print("2. Subtracting Random Numbers")
+print("3. Exit")
+print()
+option = int(input("Please choose one of the menu options: "))
+
+##### Loops program until option 3 is entered
 while option != 3:
+    
     if option == 1:
         print('test 1')
     elif option == 2:
         print('test 2')
+    elif option > 3 or option < 1:
+        print()
+        print("Not an option, please input a number presented below")
     else:
-        
-    
+        print()
+
+    print()
+    print("MAIN MENU")
+    print("-" * 30)
+    print("1. Adding Random Numbers")
+    print("2. Subtracting Random Numbers")
+    print("3. Exit")
+    print()
+    option = int(input("Please choose one of the menu options: "))
+
+##### The end of the program
+print("Thank you for playing...")
+print("Bye!!")
+

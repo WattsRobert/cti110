fa3bc7ed6e13d1dfee0ff2ff3d617192b7922f42
WattsRobert <91197142+WattsRobert@users.noreply.github.com>
2023-05-02 13:04:03 -0400

    Add files via upload
    First Upload, not finished

---
+++ P5HW_WattsRobert.py
@@ -0,0 +1,17 @@
+# Math Quiz Program
+# 5-2-2023
+# CTI-110 P5HW - Math Quiz
+# Robert Watts
+#
+
+print("Welcome to Math Quiz")
+print()
+def Math_Quiz():
+    print()
+    print("MAIN MENU")
+    print("-" * 30)
+    print("1. Adding Random Numbers")
+    print("2. Subtracting Random Numbers")
+    print("3. Exit")
+    print()
+    option = input(print("Please choose one of the menu options: "))
